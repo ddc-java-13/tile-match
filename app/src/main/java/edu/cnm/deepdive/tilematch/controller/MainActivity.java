aef51edeaf40f8641a1ca478114b68549231aa17
Marshall Eldridge <l2marshall@gmail.com>
2021-08-03 19:08:07 -0600

    Pulling data from api.

--- app/src/main/java/edu/cnm/deepdive/tilematch/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/tilematch/controller/MainActivity.java
@@ -1,12 +1,20 @@
 package edu.cnm.deepdive.tilematch.controller;
 
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.lifecycle.ViewModelProvider;
 import edu.cnm.deepdive.tilematch.R;
+import edu.cnm.deepdive.tilematch.model.entity.Gallery;
 import edu.cnm.deepdive.tilematch.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+  private ImageView image;
+  private ArrayAdapter<Gallery> adapter;
+  private Button imageSelector;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
     viewModel.loadHits();
     setContentView(R.layout.activity_main);
+
+     Button[] buttons= new Button[] {};
 
   }
 }
