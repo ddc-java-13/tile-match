7eef0a93db2f14681adacfad05cb7c766708daa0
Marshall Eldridge <l2marshall@gmail.com>
2021-08-05 09:32:02 -0600

    Tile layout completed.

--- app/src/main/java/edu/cnm/deepdive/tilematch/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/tilematch/controller/MainActivity.java
@@ -8,18 +8,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.lifecycle.ViewModelProvider;
 import edu.cnm.deepdive.tilematch.R;
+import edu.cnm.deepdive.tilematch.databinding.ActivityMainBinding;
 import edu.cnm.deepdive.tilematch.model.dto.Image;
 import edu.cnm.deepdive.tilematch.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
 
-
+private ActivityMainBinding binding;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
     getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
         WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
